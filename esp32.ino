d574bd9a377cb9b3d2d003f7bfcf140a49fa032d
SagiZiv3 <43spoq6aftmlucmgxgbrcdqy92mlgd@bots.bitbucket.org>
2023-12-25 03:25:47 +0200

    Added background task
    Added a background task to update readings from temperatures sensors

--- esp32.ino
+++ esp32.ino
@@ -1,9 +1,13 @@
 #include "src/input/buttons/ButtonHandler.h"
 #include "src/utils/TasksQueue.h"
 #include "src/utils/BackgroundTask.h"
+#include "src/input/sensors/TemperatureSensor.h"
+#include "src/input/sensors/DallasTemperatureSensors.h"
 
 ButtonHandler powerButton(GPIO_NUM_9, 10000);
 ButtonHandler resetButton(GPIO_NUM_10, 10000);
+DallasTemperatureSensors dallasTempSensors(GPIO_NUM_3);
+TimedCachingTemperatureSensor temperatureSensor(dallasTempSensors, 15000);
 
 void initializeButtons()
 {
@@ -30,10 +34,21 @@ void initializeButtons()
   TasksQueue::getInstance().enqueueTask(resetButtonTask);
 }
 
+void initializeTemperatureSensor()
+{
+  temperatureSensor.init();
+  // Update the temperatures every 10 seconds (TODO: define a variable for that)
+  static BackgroundTask tempsUpdateTask([]()
+                                    { temperatureSensor.refreshData(false); },
+                                    10000);
+  TasksQueue::getInstance().enqueueTask(tempsUpdateTask);
+}
+
 void setup()
 {
-  TasksQueue::getInstance().initialize(ARDUINO_RUNNING_CORE);
   initializeButtons();
+  initializeTemperatureSensor();
+  TasksQueue::getInstance().initialize(ARDUINO_RUNNING_CORE);
 }
 
 void loop()
